2f83e0312ccf1469579fba395c36213843364de3
Nicole Hamilton <andromeda@Nicoles-Air-2.lan>
2023-10-14 15:28:56 -0700

    set up template inheritance with static css

--- catcollector_project/catcollector_app/views.py
+++ catcollector_project/catcollector_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'cats/home.html') 
+    # return res.render('home.ejs', context)
+
+def about(request):
+    return render(request, 'cats/about.html')
