07b207c15414dc93f26ccdbd02aed68b42737a81
Nicole Hamilton <andromeda@Nicoles-Air-2.lan>
2023-10-14 16:06:13 -0700

    cats index create

--- catcollector_project/catcollector_app/views.py
+++ catcollector_project/catcollector_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Add this cats list below the imports
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 0},
+]
+
+
 # Create your views here.
 def home(request):
     return render(request, 'cats/home.html') 
@@ -8,3 +15,6 @@ def home(request):
 
 def about(request):
     return render(request, 'cats/about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
