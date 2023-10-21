d80889f5ad3369cca7b04164508d38a4772af64e
Nicole Hamilton <andromeda@Nicoles-Air-2.lan>
2023-10-21 11:16:34 -0700

    set up feeding model

--- catcollector_project/catcollector_app/models.py
+++ catcollector_project/catcollector_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -13,4 +20,35 @@ class Cat(models.Model):
     # handle redirecting for update and create
     def get_absolute_url(self):
         return reverse('detail', kwargs={'cat_id': self.id})
-    
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field aka our MEALS tuple variable
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+    )
+    # syntax / column made for FK = (Model)_id
+    # in the db the column is called cat_id
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # syntax for getting our meal visuals get_(Model)_display()
+        return f"{self.get_meal_display()} on {self.date}"
+
+
+# feeding_first.cat.description
+
+# feeding_first = {
+#     date = '2023-10-06',
+#     meal = 'B'
+#     cat = {
+#         name = rubber biscuit
+#         desciption = laskdjfs;aldkjf
+#         age = 5
+#         breed = lkasdjf
+#     }
+# }    
+
